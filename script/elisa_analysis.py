a3e3c9c3f8d750a2d5007ef204da3b3c8ce171ea
korMichi <mkorenkov52@yahoo.com>
2020-06-29 21:53:57 +0200

    added functionality to analysis function

--- script/elisa_analysis.py
+++ script/elisa_analysis.py
@@ -55,10 +55,10 @@ def get_standard(array, i):
         for n in range(len(array[x])):
             if axis == 1:
                 standard_array[x].append(array[x][n].iloc[0:8, 0].to_numpy())
-                value_array[x].append(array[x][n].drop(1, axis=1).to_numpy())
+                value_array[x].append(array[x][n].drop(1, axis=1))
             elif axis == 2:
                 standard_array[x].append(array[x][n].iloc[[0]].to_numpy())
-                value_array[x].append(array[x][n].drop(["H"]).to_numpy())
+                value_array[x].append(array[x][n].drop(["H"]))
     return value_array, standard_array
 
 
@@ -68,21 +68,64 @@ def analysis(standard, values, plates):
     finding the clostest partner
     """
     all_means = []
+    standard_dict = {}  # all standard value computing (mean of sum of means) saved here
+    export_standard_dict = {}  # for easy returnability of standard dictionary and keys
+    value_dict = {}  # for easy returnability of value dictionary and keys
     for x in range(plates):
         for n in range(len(standard[x])):
             all_means.append(standard[x][n].mean())
     
     mean_standard = [list(i) for i in zip(*[all_means[i:i+plates] for i in range(0, len(all_means), plates)])]
 
-    for x in range(plates):
-        if x == plates - 1:  # for loop breakes before last loop
-            break
-        else:
-            for n in range(len(standard[x])):
-                for i in range(len(standard[x])):
-                    differences = {"{0}-{1}".format(x,n) : np.array(standard_array[x][n]) - np.array(standard_array[x+1][i])}
-    print(mean_standard)
-    return differences
+    for x in range(plates):  # Section checks how many input platesband calculates the mean of the mean of the standard 
+        if plates == 1:
+            standard_dict[x] = mean_standard[x]
+        if plates == 2:
+            if x == plates - 1:  # for loop breakes before last loop
+                break
+            else:
+                for n in range(len(mean_standard[x])):
+                    for i in range(len(mean_standard[x])):
+                        standard_dict[(x, n), (x+1, i)] =  (np.array(mean_standard[x][n]) + np.array(mean_standard[x+1][i]))/plates
+        if plates == 3:
+            if x == plates - 1:  # for loop breakes before last loop
+                break
+            else:
+                for n in range(len(mean_standard[x])):
+                    for i in range(len(mean_standard[x])):
+                        standard_dict[(x, n), (x+1, i), (x+2, i)] =  (np.array(mean_standard[x][n]) + np.array(mean_standard[x+1][i]) + np.array(mean_standard[x+2][i]))/plates
+        if plates == 4:
+            if x == plates - 1:  # for loop breakes before last loop
+                break
+            else:
+                for n in range(len(mean_standard[x])):
+                    for i in range(len(mean_standard[x])):
+                        standard_dict[(x, n), (x+1, i), (x+2,i), (x+3, i)] =  (np.array(mean_standard[x][n]) + np.array(mean_standard[x+1][i]) + np.array(mean_standard[x+2][i]) + np.array(mean_standard[x+3][i]))/plates
+
+    for x in range(plates):  # Section to convert value array into a dictionary for easy return
+        for n in range(len(values[x])):
+            value_dict[(x, n)] = values[x][n]
+    
+    for x in range(plates):  # Section to convert standard array into a dictionary for easy return
+        for n in range(len(standard[x])):
+            export_standard_dict[(x, n)] = standard[x][n]
+
+    min_key_pair = min(standard_dict, key=standard_dict.get)  # computes the key for the lowest mean pair
+    key_a, key_b = min_key_pair
+    print(key_a, key_b)
+    
+    if plates == 1:
+        key_a = min_key_pair
+        return export_standard_dict[key_a], value_dict[key_a]
+    if plates == 2:
+        key_a, key_b = min_key_pair
+        return export_standard_dict[key_a], value_dict[key_a], export_standard_dict[key_b], value_dict[key_b]
+    if plates == 3:
+        key_a, key_b, key_c = min_key_pair
+        return export_standard_dict[key_a], value_dict[key_a], export_standard_dict[key_b], value_dict[key_b], export_standard_dict[key_c], value_dict[key_c]
+    if plates == 4:
+        key_a, key_b, key_c, key_d = min_key_pair
+        return export_standard_dict[key_a], value_dict[key_a], export_standard_dict[key_b], value_dict[key_b], export_standard_dict[key_c], value_dict[key_c], export_standard_dict[key_d],value_dict[key_d]
 
 
 def write_output(filename, standard_array, value_array):
@@ -111,8 +154,7 @@ excel_file = input("Please enter the name of the excel file: ")
 plates = int(input("Please enter the number of plates: "))
 data = list(load_data(excel_file, plates))
 value_array, standard_array = get_standard(data, plates)
-
-
 number_of_sheet = analysis(standard_array, value_array, plates)
+print(number_of_sheet)
 #print(type(standard_array[0][0]))
 #write_output(input("Filename? "), standard_array, value_array)
